76ec8f018bb01a21460e15a154490684ba1e8c84
Shivam Lakhani <shivamlakhani03@gmail.com>
2025-07-13 20:47:37 +0100

    Stage and commit changes

--- src/main.py
+++ src/main.py
@@ -1,19 +1,28 @@
 from textnode import TextNode, TextType
 from gencontent import generate_pages_recursive
 
+
 def main():
     node=TextNode('This is some anchor text', TextType.LINK, 'https://www.boot.dev')
-    copy_directory("static", "public")
+    copy_directory("static", "docs")
     print("Copying static files to public directory...")
     copy_directory(dir_path_static, dir_path_public)
 
     print("Generating page...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public,basepath)
+
+
+
+import sys
+if len(sys.argv) > 1:
+    basepath = sys.argv[1]
+else:
+    basepath = "/"
 
 import os
 import shutil
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
